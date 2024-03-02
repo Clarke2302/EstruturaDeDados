5d8bff6b5e5b84d89281bcdab7d675e6b4cb97de
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2024-03-02 16:18:08 -0300

    Create Nodo.java

---
+++ 1_programaPilhasFilasListas/Nodo.java
@@ -0,0 +1,11 @@
+class Nodo {
+    int valor;
+    Nodo proximo;
+    Nodo anterior;
+
+    public Nodo(int valor) {
+        this.valor = valor;
+        this.proximo = null;
+        this.anterior = null;
+    }
+}
